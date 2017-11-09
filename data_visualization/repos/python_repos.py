8811a0ec0707dbebab5192628156130409d5b681
jeremyyang <jeremyyang824@aliyun.com>
2017-11-09 20:11:26 +0800

    repos ok

---
+++ data_visualization/repos/python_repos.py
@@ -0,0 +1,46 @@
+import pygal
+import requests
+from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS
+
+# 执行API调用并存储响应
+url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
+r = requests.get(url)
+print("Status code:", r.status_code)
+
+# 将API响应存储在一个变量中
+response_dict = r.json()
+print("Total repositories:", response_dict['total_count'])
+
+repo_dicts = response_dict['items']
+
+names, stars = [], []
+for repo_dict in repo_dicts:
+    names.append(repo_dict['name'])
+    stars.append(repo_dict['stargazers_count'])
+
+# 可视化
+my_style = LS('#333366', base_style=LCS)
+
+my_config = pygal.Config()
+my_config.x_label_rotation = 45
+my_config.show_legend = False
+my_config.title_font_size = 24
+my_config.label_font_size = 14
+my_config.major_label_font_size = 18
+my_config.truncate_label = 15
+my_config.show_y_guides = False
+my_config.width = 1000
+
+chart = pygal.Bar(my_config, style=my_style)
+chart.title = 'Most-Starred Python Projects on GitHub'
+chart.x_labels = names  # ['httpie', 'django', 'flask']
+
+#
+# plot_dicts = [
+#     {'value': 16101, 'label': 'Description of httpie.'},
+#     {'value': 15028, 'label': 'Description of django.'},
+#     {'value': 14798, 'label': 'Description of flask.'}
+# ]
+
+chart.add('', stars)
+chart.render_to_file('python_repos.svg')
